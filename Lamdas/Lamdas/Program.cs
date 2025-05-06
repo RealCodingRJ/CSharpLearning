48dba37bfa7996ce20908d1aa45e566b8a85e804
Ryan James <realcoder2023@gmail.com>
2025-05-06 12:34:42 -0400

    added c# code

---
+++ Lamdas/Lamdas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lamdas
+{
+    class Program
+    {
+        public static void Main(string[] _)
+        {
+            static string Name(String name) => name;
+
+
+            Console.WriteLine(Name("Ryan"));
+            Console.ReadLine();
+
+        }
+    }
+}
