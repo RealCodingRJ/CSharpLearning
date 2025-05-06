0ac9c21751046378cbf2b44d4fe543ba3aed0125
Ryan James <realcoder2023@gmail.com>
2025-05-06 13:29:19 -0400

    added threading code

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Threading
+{
+    class Program
+    {
+        public static void printName()
+        {
+            Console.WriteLine($"Hello Ryan");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] _)
+        {
+          
+            
+            Thread th = new Thread(printName);
+            th.Start();
+
+        }
+    }
+}
